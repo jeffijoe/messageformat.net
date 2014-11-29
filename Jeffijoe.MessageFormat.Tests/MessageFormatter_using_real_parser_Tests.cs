4a9870d1eeed1572e98f6550aeb353c5dd206585
Jeff Hansen <jeffijoe@hotmail.com>
2014-11-29 03:34:18 +0100

    Added some benchmarking.

--- Jeffijoe.MessageFormat.Tests/MessageFormatter_using_real_parser_Tests.cs
+++ Jeffijoe.MessageFormat.Tests/MessageFormatter_using_real_parser_Tests.cs
@@ -8,6 +8,7 @@
 using System.Collections.Generic;
 using Jeffijoe.MessageFormat.Formatting;
 using Jeffijoe.MessageFormat.Parsing;
+using Jeffijoe.MessageFormat.Tests.TestHelpers;
 using Moq;
 using Xunit;
 using Xunit.Extensions;
@@ -17,11 +18,17 @@ namespace Jeffijoe.MessageFormat.Tests
     public class MessageFormatter_using_real_parser_Tests
     {
         [Theory]
-        [InlineData(@"Hi, I'm {name}, and it's still {name,plural,whatever
+        [InlineData(@"Hi, I'm {name}, and it's still {name, plural, whatever
 
 i do what i want
 
 still inside braces
+
+bad boiis bad boiis
+
+whatchu gonna do?
+
+whatchu gonna do when dey come for youu?
 
 }, ok?", "Hi, I'm Jeff, and it's still Jeff, ok?")]
         public void FormatMessage_using_real_parser_and_library_mock(string source, string expected)
@@ -34,6 +41,19 @@ still inside braces
             args.Add("name", "Jeff");
             dummyFormatter.Setup(x => x.Format("en", It.IsAny<FormatterRequest>(), args, subject)).Returns("Jeff");
             mockLibary.Setup(x => x.GetFormatter(It.IsAny<FormatterRequest>())).Returns(dummyFormatter.Object);
+
+
+            // Warm up
+            Benchmark.Start("Warm-up");
+            subject.FormatMessage(source, args);
+            Benchmark.End();
+
+            Benchmark.Start("Aaaand a few after warm-up");
+            for (int i = 0; i < 1000; i++)
+            {
+                subject.FormatMessage(source, args);
+            }
+            Benchmark.End();
 
             Assert.Equal(expected, subject.FormatMessage(source, args));
         }
