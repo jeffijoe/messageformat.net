054ff1d7d5c24da32afaebac78102c8241ca2b50
Jeff Hansen <jeffijoe@hotmail.com>
2015-08-19 23:53:52 +0200

    Signed the assembly.

--- src/Jeffijoe.MessageFormat/Properties/AssemblyInfo.cs
+++ src/Jeffijoe.MessageFormat/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: InternalsVisibleTo("Jeffijoe.MessageFormat.Tests")]
+[assembly: InternalsVisibleTo("Jeffijoe.MessageFormat.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001004f0dc10ad8873751f986416a7d3134e473ad3454a7138e26cf9760eff341709fc50e69d985b4e0ea512b5628079043a31ce1e402f4eaebffb5772eed9ef3a7a9e39f122633f19529a1e9988005289ed09a1e294abe13152afebc59835c2fef2879d8641b564daa7f511298ec2f8a3e9b322819e05cbaea0bfc73fe100615bfc7")]
 
 // Version information for an assembly consists of the following four values:
 //      Major Version
