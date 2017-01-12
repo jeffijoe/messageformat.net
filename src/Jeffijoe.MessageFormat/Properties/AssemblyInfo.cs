df8112e9381245bdcde3d8008d9c2b995d6ed9f1
Jeff Hansen <jeffijoe@hotmail.com>
2017-01-12 13:09:23 +0100

    what year is this... -_-

--- src/Jeffijoe.MessageFormat/Properties/AssemblyInfo.cs
+++ src/Jeffijoe.MessageFormat/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Jeffijoe.MessageFormat")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
