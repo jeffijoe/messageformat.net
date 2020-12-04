cf4e52962e2f7a2c95f45dc46195ec54d788598b
Jeff Hansen <jeffijoe@hotmail.com>
2020-12-03 22:37:17 -0500

    Simplify AssemblyInfo

--- src/Jeffijoe.MessageFormat/Properties/AssemblyInfo.cs
+++ src/Jeffijoe.MessageFormat/Properties/AssemblyInfo.cs
@@ -7,27 +7,4 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Jeffijoe.MessageFormat")]
-[assembly: AssemblyDescription("ICU Message Format for .NET.\r\n\r\nCheck the README on Github for more information.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MessageFormatter for .NET")]
-[assembly: AssemblyCopyright("Copyright © Jeff Hansen 2014 to present")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("Jeffijoe.MessageFormat.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001004f0dc10ad8873751f986416a7d3134e473ad3454a7138e26cf9760eff341709fc50e69d985b4e0ea512b5628079043a31ce1e402f4eaebffb5772eed9ef3a7a9e39f122633f19529a1e9988005289ed09a1e294abe13152afebc59835c2fef2879d8641b564daa7f511298ec2f8a3e9b322819e05cbaea0bfc73fe100615bfc7")]
-
-// Version information for an assembly consists of the following four values:
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
