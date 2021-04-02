77915c933a58e28f1f7029c8a362683324f58261
Kostiantyn Sharovarskyi <kostiantyn@sharovarskyi.com>
2021-04-03 00:49:27 +0300

    Remove not used code

--- src/Jeffijoe.MessageFormat/Formatting/Formatters/PluralRulesMetadata.cs
+++ src/Jeffijoe.MessageFormat/Formatting/Formatters/PluralRulesMetadata.cs
@@ -6,21 +6,6 @@ namespace Jeffijoe.MessageFormat.Formatting.Formatters
 {
     public static partial class PluralRulesMetadata
     {
-        public static string DefaultPluralRule(double number)
-        {
-            if(number == 0)
-            {
-                return "zero";
-            }
-
-            if(number == 1)
-            {
-                return "one";
-            }
-
-            return "other";
-        }
-
         public static partial bool TryGetRuleByLocale(string locale, out Pluralizer pluralizer);
     }
 }
