0152076146957feb7a5d5e4b0ff02740f2e74650
Kostiantyn Sharovarskyi <kostiantyn@sharovarskyi.com>
2021-04-26 03:47:08 +0300

    Remove double parsing

--- src/Jeffijoe.MessageFormat/Formatting/Formatters/PluralContext.cs
+++ src/Jeffijoe.MessageFormat/Formatting/Formatters/PluralContext.cs
@@ -5,11 +5,6 @@ namespace Jeffijoe.MessageFormat.Formatting.Formatters
 {
     public readonly struct PluralContext
     {
-        public PluralContext(decimal number) : this(number.ToString(CultureInfo.InvariantCulture))
-        {
-
-        }
-
         public PluralContext(int number)
         {
             Number = number;
@@ -22,18 +17,24 @@ namespace Jeffijoe.MessageFormat.Formatting.Formatters
             C = 0;
             E = 0;
         }
-
-        public PluralContext(string number)
+        
+        public PluralContext(decimal number) : this(number.ToString(CultureInfo.InvariantCulture), (double) number)
         {
-            var dotIndex = number.IndexOf(".", StringComparison.InvariantCulture);
+        }
 
+        public PluralContext(string number) : this(number, double.Parse(number))
+        {
+        }
+
+        private PluralContext(string number, double parsed)
+        {
+            Number = parsed;
+            N = Math.Abs(parsed);
+            I = (int) parsed;
+
+            var dotIndex = number.IndexOf(".", StringComparison.InvariantCulture);
             if (dotIndex == -1)
             {
-                var parsed = int.Parse(number);
-
-                Number = parsed;
-                N = Math.Abs(parsed);
-                I = parsed;
                 V = 0;
                 W = 0;
                 F = 0;
@@ -50,16 +51,12 @@ namespace Jeffijoe.MessageFormat.Formatting.Formatters
                 var fractionSpan = number.Substring(dotIndex + 1, number.Length - dotIndex - 1);
                 var fractionSpanWithoutZeroes = fractionSpan.TrimEnd('0');
 #endif
-                var parsed = double.Parse(number);
 
-                Number = parsed;
-                N = Math.Abs(parsed);
-                I = (int)parsed;
                 V = fractionSpan.Length;
                 W = fractionSpanWithoutZeroes.Length;
                 F = int.Parse(fractionSpan);
                 T = int.Parse(fractionSpanWithoutZeroes);
-                    C = 0;
+                C = 0;
                 E = 0;
             }
         }
