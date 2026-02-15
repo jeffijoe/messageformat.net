d177f40ce42f867376841a7d354aa1da63897c38
Steven Fuqua <sfuqua@microsoft.com>
2026-02-15 15:38:43 -0800

    Add some documentation to LMDL types

--- src/Jeffijoe.MessageFormat.MetadataGenerator/Plural/Parsing/AST/Condition.cs
+++ src/Jeffijoe.MessageFormat.MetadataGenerator/Plural/Parsing/AST/Condition.cs
@@ -4,11 +4,18 @@ using System.Diagnostics;
 namespace Jeffijoe.MessageFormat.MetadataGenerator.Plural.Parsing.AST;
 
 /// <summary>
-/// Corresponds to a pluralRule tag in CLDR XML.
+/// Represents the 'condition' part of the <see href="https://unicode.org/reports/tr35/tr35-numbers.html#plural-rules-syntax">LDML grammar</see>.
 /// </summary>
 /// <example>
+/// Given the following 'pluralRule' tag:
 /// &lt;pluralRule count="one"&gt;i = 1 and v = 0 @integer 1&lt;/pluralRule&gt;
+///
+/// A Condition instance would represent 'i = 1 and v = 0' as a single <see cref="OrCondition"/>.
 /// </example>
+/// <remarks>
+/// The grammar defines a condition as a union of 'and_conditions', which we model as a
+/// list of <see cref="OrCondition"/> that each internally tracks <see cref="OrCondition.AndConditions"/>.
+/// </remarks>
 [DebuggerDisplay("{{RuleDescription}}")]
 public class Condition
 {
@@ -27,6 +34,10 @@ public class Condition
     /// <summary>
     /// The original text of this rule, e.g., "i = 1 and v = 0 @integer 1".
     /// </summary>
+    /// <remarks>
+    /// Note - this includes the sample text ('@integer 1') which gets stripped out
+    /// when parsing the rule's conditional logic.
+    /// </remarks>
     public string RuleDescription { get; }
 
     /// <summary>
--- src/Jeffijoe.MessageFormat/Formatting/Formatters/PluralContext.cs
+++ src/Jeffijoe.MessageFormat/Formatting/Formatters/PluralContext.cs
@@ -3,6 +3,9 @@ using System.Globalization;
 
 namespace Jeffijoe.MessageFormat.Formatting.Formatters;
 
+/// <summary>
+/// Represents the 'operations' for a given source number, as defined by <see href="https://unicode.org/reports/tr35/tr35-numbers.html#operands">Unicode TR35/LDML</see>.
+/// </summary>
 internal readonly struct PluralContext
 {
     public PluralContext(int number)
@@ -26,10 +29,24 @@ internal readonly struct PluralContext
     {
     }
 
+    /// <summary>
+    /// Represents operands for a source number in string format.
+    /// This library treats the input as a stringified double and does not currently parse out
+    /// compact decimal forms (e.g., "1.25c4").
+    /// </summary>
     public PluralContext(string number) : this(number, double.Parse(number, CultureInfo.InvariantCulture))
     {
     }
 
+    /// <summary>
+    /// Common constructor for parsing out operands from a stringified number.
+    /// </summary>
+    /// <remarks>
+    /// The values of <see cref="V"/>, <see cref="W"/>, <see cref="F"/>, and <see cref="T"/> are all derived
+    /// from the fractional part of the number, so it's important <paramref name="number"/> be parsable as a number.
+    /// </remarks>
+    /// <param name="number">The number in string form, as a decimal (not scientific/compact form).</param>
+    /// <param name="parsed">The number pre-parsed as a double.</param>
     private PluralContext(string number, double parsed)
     {
         Number = parsed;
@@ -60,26 +77,77 @@ internal readonly struct PluralContext
             W = fractionSpanWithoutZeroes.Length;
             F = int.Parse(fractionSpan);
             T = int.Parse(fractionSpanWithoutZeroes);
+
+            // The compact decimal exponent representations are not used in this library as operands are
+            // always assumed to be parsable numbers.
             C = 0;
             E = 0;
         }
     }
 
+    /// <summary>
+    /// The 'source number' being evaluated for pluralization.
+    /// </summary>
     public double Number { get; }
 
+    /// <summary>
+    /// The absolute value of <see cref="Number"/>.
+    /// </summary>
     public double N { get; }
 
+    /// <summary>
+    /// The integer digits of <see cref="Number"/>.
+    /// </summary>
+    /// <example>
+    /// 22.6 -> I = 22
+    /// </example>
     public int I { get; }
 
+    /// <summary>
+    /// The count of visible fraction digits of <see cref="Number"/>, with trailing zeroes.
+    /// </summary>
+    /// <example>
+    /// 1.450 -> V = 3
+    /// </example>
     public int V { get; }
 
+    /// <summary>
+    /// The count of visible fraction digits of <see cref="Number"/>, without trailing zeroes.
+    /// </summary>
+    /// <example>
+    /// 1.450 -> W = 2
+    /// </example>
     public int W { get; }
 
+    /// <summary>
+    /// The visible fraction digits of <see cref="Number"/>, with trailing zeroes, as an integer.
+    /// </summary>
+    /// <example>
+    /// 1.450 -> F = 450
+    /// </example>
     public int F { get; }
 
+    /// <summary>
+    /// The visible fraction digits of <see cref="Number"/>, without trailing zeroes, as an integer.
+    /// </summary>
+    /// <example>
+    /// 1.450 -> T = 45
+    /// </example>
     public int T { get; }
 
+    /// <summary>
+    /// The compact decimal exponent of <see cref="Number"/>, in such cases where <see cref="Number"/>
+    /// is represented as "[x]cC" such that <see cref="Number"/> == x * 10^C.
+    /// </summary>
+    /// <example>
+    /// 1.25c4 -> C = 4
+    /// 125c2 -> C = 2
+    /// 12500 -> C = 0, as the number is not represented in compact decimal form.
+    /// </example>
     public int C { get; }
 
+    /// <summary>
+    /// Deprecated (in LDML) synonym for <see cref="C"/>, reserved for future use by the standard.
+    /// </summary>
     public int E { get; }
 }
