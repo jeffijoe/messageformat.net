f707fecc492aeb924e6ad4ca57b91b969e7aec1e
Steven Fuqua <sfuqua@microsoft.com>
2026-02-15 01:21:46 -0800

    xmldoc for AST types

--- src/Jeffijoe.MessageFormat.MetadataGenerator/Plural/Parsing/AST/Condition.cs
+++ src/Jeffijoe.MessageFormat.MetadataGenerator/Plural/Parsing/AST/Condition.cs
@@ -3,6 +3,12 @@ using System.Diagnostics;
 
 namespace Jeffijoe.MessageFormat.MetadataGenerator.Plural.Parsing.AST;
 
+/// <summary>
+/// Corresponds to a pluralRule tag in CLDR XML.
+/// </summary>
+/// <example>
+/// &lt;pluralRule count="one"&gt;i = 1 and v = 0 @integer 1&lt;/pluralRule&gt;
+/// </example>
 [DebuggerDisplay("{{RuleDescription}}")]
 public class Condition
 {
@@ -13,9 +19,18 @@ public class Condition
         OrConditions = orConditions;
     }
 
+    /// <summary>
+    /// The plural form this condition or rule defines, e.g., "one", "two", "few", "many", "other".
+    /// </summary>
     public string Count { get; }
 
+    /// <summary>
+    /// The original text of this rule, e.g., "i = 1 and v = 0 @integer 1".
+    /// </summary>
     public string RuleDescription { get; }
 
+    /// <summary>
+    /// Parsed representation of <see cref="RuleDescription"/>.
+    /// </summary>
     public IReadOnlyList<OrCondition> OrConditions { get; }
 }
--- src/Jeffijoe.MessageFormat.MetadataGenerator/Plural/Parsing/AST/PluralRule.cs
+++ src/Jeffijoe.MessageFormat.MetadataGenerator/Plural/Parsing/AST/PluralRule.cs
@@ -2,6 +2,16 @@
 
 namespace Jeffijoe.MessageFormat.MetadataGenerator.Plural.Parsing.AST;
 
+/// <summary>
+/// Corresponds to a pluralRules tag in CLDR XML (not to be confused with pluralRule).
+/// Each instance of this class represents multiple individual rules for a set of locales.
+/// </summary>
+/// <example>
+///     &lt;pluralRules locales="ast de en et fi fy gl ia ie io ji lij nl sc sv sw ur yi"&gt;
+///         &lt;pluralRule count = "one"&gt; i = 1 and v = 0 @integer 1&lt;/pluralRule&gt;
+///         ...
+///     &lt;/pluralRules&gt;
+/// </example>
 public class PluralRule
 {
     public PluralRule(string[] locales, IReadOnlyList<Condition> conditions)
